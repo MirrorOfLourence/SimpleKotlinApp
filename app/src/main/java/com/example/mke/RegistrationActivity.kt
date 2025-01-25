419a4ac030d74deb6ae872324755d949f29dd2d8
MirrorOfLourence <90408662+MirrorOfLourence@users.noreply.github.com>
2025-01-25 19:37:22 +0200

    Add task 3

--- app/src/main/java/com/example/mke/RegistrationActivity.kt
+++ app/src/main/java/com/example/mke/RegistrationActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mke
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -11,11 +13,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.regex.Pattern
 
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -77,8 +79,13 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
-                    // Valid input
-                    // Proceed with next steps (currently nothing happens)
+                    val sharedPrefsUtils = SharedPrefsUtils(this)
+                    sharedPrefsUtils.writeCredentialsToSharedPrefs(
+                        editNumberOrEmail.inputType,
+                        input,
+                        password
+                    )
+                    startActivity(Intent(this, ContentActivity::class.java))
                 }
             }
         }
